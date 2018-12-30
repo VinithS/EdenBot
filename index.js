12d6cc7c3a96539d1223197dc04963edb6135c84
Vinith Sharma <vinith.sharma13@gmail.com>
2018-12-30 15:12:21 -0600

    log-in and basic "ping" command

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const settings = require("./resources/settings.json");
+const Discord = require("discord.js")
 
-console.log(settings);
+const Client = new Discord.Client();
+
+const config = require("./resources/config.json");
+
+console.log(config);
+
+
+Client.on("ready", () => {
+    console.log("Bot online");
+});
+   
+Client.on("message", (message) => {
+    if (message.content.startsWith("ping")) {
+        message.channel.send("pong!");
+    }
+});
+
+Client.login(config.token);
 
 // TODO: create command hierarchy/structure
