a3310a85c4797f88b3d098e85591b1dafa69fa50
Vinith Sharma <vinith.sharma13@gmail.com>
2018-12-30 23:47:50 -0600

    message when joining a server

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ client.once("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+// When joining a server for the first time, give a thank you message in the general channel
+client.on("guildCreate", guild => {
+    let channelID;
+    let channels = guild.channels;
+    channelLoop:
+    for (let c of channels) {
+        let channelType = c[1].type;
+        if (channelType === "general") {
+            channelID = c[0];
+            break channelLoop;
+        }
+    }
+
+    let channel = client.channels.get(guild.systemChannelID || channelID);
+    channel.send('I have arrived!');
+});
 
 client.on("message", (message) => {
     // has to invoke the bot using it's prefix + bot's own message
